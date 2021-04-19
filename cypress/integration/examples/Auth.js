30fca85f173f67b3ce5f44da699043b583a58ddf
Umi <umy.ikem@gmail.com>
2021-04-19 23:17:10 +0100

    add invalid authentication tests

--- cypress/integration/examples/Auth.js
+++ cypress/integration/examples/Auth.js
@@ -1,8 +1,28 @@
 /// <reference types="Cypress" />
 
 describe('Auth Tests', () => {
-    it('Login with valid username & password', () => {
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/')
+      })
+    
+    it('Validate error message when password is empty', () => {
+        cy.get('#user-name', { timeout: 4000 })
+        .type('standard_user')
+        cy.get('#login-button').click()
+        cy.get('h3[data-test="error"]').should('exist').and('have.text', 'Epic sadface: Password is required')
+    })  
+
+
+    it('Validate error message when password is invalid', () => {
+        cy.get('#user-name', { timeout: 4000 })
+        .type('standard_user')
+        cy.get('#password').type('secret_sauce100')
+        cy.get('#login-button').click()
+        cy.get('h3[data-test="error"]').should('exist').and('have.text', 'Epic sadface: Username and password do not match any user in this service')
+    })
+
+
+    it('Login with valid username & password', () => {
         cy.get('#user-name', { timeout: 5000 })
         .type('standard_user')
         cy.get('#password').type('secret_sauce')
@@ -10,4 +30,6 @@ describe('Auth Tests', () => {
         cy.get('#react-burger-menu-btn')
         .should('be.visible')
     })
+
+
 })
--- cypress/testcases.md
+++ cypress/testcases.md
@@ -1,3 +1,5 @@
 ### These test cases are for tests on the sauce demo website - [title] (https://www.saucedemo.com/)
 
 - Login with valid username & password
+- Validate error message when password is invalid
+- Validate error message when password is empty
