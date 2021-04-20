d21c0870089d103c2eca0140b8bc5f66c59841d3
Umi <umy.ikem@gmail.com>
2021-04-21 00:56:02 +0100

    implemented first add to cart test

--- cypress/integration/tests/add-to-cart.spec.js
+++ cypress/integration/tests/add-to-cart.spec.js
@@ -11,6 +11,17 @@ describe('Add to Cart Scenarios', () => {
       })
 
     it('Add 1st item in list to cart & logout', () => {
-        
+
+        cy.get('.inventory_list').each(($el, $index, $list) => {
+            $el
+            .first()
+            .find('.inventory_item_description > .inventory_item_label > #item_4_title_link .inventory_item_name').click()
+        })
+        cy.get('[data-test=add-to-cart-sauce-labs-backpack]').click()
+        cy.get('span.shopping_cart_badge').should('have.text', '1')
+        cy.get('#react-burger-menu-btn').click()
+        cy.get('#logout_sidebar_link').click()
+
     })
-})
+    
+}) 
